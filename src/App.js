528ff4c27746839609c1c8bb7c171ad0986542e3
Manulapo <you@example.com>
2023-09-12 20:16:16 +0200

    m

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './routes/home/home.component';
+import Home from './routes/home/home.component.jsx';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
